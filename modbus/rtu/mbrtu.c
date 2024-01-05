f681f386831183f8b7e540f784ebd4a9c11f201b
Christian Meusel <christian.meusel@posteo.de>
2024-01-05 17:20:34 +0100

    Fix assertion in eMBRTUReceive
    Having a completely filled receive buffer is no issue per se. This check
    should be the same as in the port's serial receive interrupt handler.

--- modbus/rtu/mbrtu.c
+++ modbus/rtu/mbrtu.c
@@ -153,7 +153,7 @@ eMBRTUReceive( UCHAR * pucRcvAddress, UCHAR ** pucFrame, USHORT * pusLength )
     eMBErrorCode    eStatus = MB_ENOERR;
 
     ENTER_CRITICAL_SECTION(  );
-    assert( usRcvBufferPos < MB_SER_PDU_SIZE_MAX );
+    assert( usRcvBufferPos <= MB_SER_PDU_SIZE_MAX );
 
     /* Length and CRC check */
     if( ( usRcvBufferPos >= MB_SER_PDU_SIZE_MIN )
